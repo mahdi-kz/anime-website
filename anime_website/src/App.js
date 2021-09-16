da355e6f828dbb3cfd36c6e7695f9220b0604cfb
mahdi <m.karimzadeh@ehsan-group.com>
2021-09-16 21:54:48 +0430

    Add includeHTML.js to split HTML files into different files.

--- anime_website/src/App.js
+++ anime_website/src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header >
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
