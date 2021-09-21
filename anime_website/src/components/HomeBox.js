deb990a2dc119c9309a81614d81a46af98b2b594
mahtab <karbasiMahtab@gmail.com>
2021-09-21 12:00:26 +0430

    adding boxes css

--- anime_website/src/components/HomeBox.js
+++ anime_website/src/components/HomeBox.js
@@ -10,37 +10,39 @@ import image4 from '../images/4.jpeg';
 
 function HomeBox(){
 
-	const [selected, setSelected] = useState(null);
 	const [title, setTitle] = useState('Our Items');
-	
+	const [selectedBox1, setSelectedBox1] = useState(false);
+	const [selectedBox2, setSelectedBox2] = useState(false);
+	const [selectedBox3, setSelectedBox3] = useState(false);
+	const [selectedBox4, setSelectedBox4] = useState(false);
 
 	return(
 		<>
 			<ul>
-				<li className="top-li"  
-					onMouseEnter={()=>setTitle("image 1")} 
-					onMouseLeave={()=>setTitle('Our Items')}
+				<li className={selectedBox1?"firstBoxHover":"firstBox"}  
+					onMouseEnter={()=>{setTitle("image 1");setSelectedBox1(true)}} 
+					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox1(false)}}
 				>
-					<img src={image1} alt="" />
+					<img className="top-li" src={image1} alt="" />
 				</li>
-				<li className="bottom-li" 
-					onMouseEnter={()=>setTitle("image 2")} 
-					onMouseLeave={()=>setTitle('Our Items')}
+				<li className={selectedBox2?"secondBoxHover":"secondBox"}
+					onMouseEnter={()=>{setTitle("image 2");setSelectedBox2(true)}} 
+					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox2(false)}}
 				>
 					<img src={image2} alt="" />
 				</li>
-				<li className="top-li" 
-					onMouseEnter={()=>setTitle("image 3")} 
-					onMouseLeave={()=>setTitle('Our Items')}
+				<li className={selectedBox3?"thirdBoxHover":"thirdBox"}
+					onMouseEnter={()=>{setTitle("image 3");setSelectedBox3(true)}} 
+					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox3(false)}}
 				>
-					<img src={image3} alt="" />
+					<img className="top-li" src={image3} alt="" />
 				</li>
-				<li className="bottom-li" 
-					onMouseEnter={()=>setTitle("image 4")} 
-					onMouseLeave={()=>setTitle('Our Items')}
+				<li className={selectedBox4?"fourthBoxHover":"fourthBox"}
+					onMouseEnter={()=>{setTitle("image 4");setSelectedBox4(true)}} 
+					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox4(false)}}
 				>
 					<img src={image4} alt="" />
-				</li>
+				</li> 
 			</ul>
 			<h2 className="title">{title}</h2>
 		</>
