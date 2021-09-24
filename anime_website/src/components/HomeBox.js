ae26cf396f170336db7270d79ebe19bdf352a7be
mahdi <m.karimzadeh@ehsan-group.com>
2021-09-24 14:50:07 +0330

    Change Home Buttons

--- anime_website/src/components/HomeBox.js
+++ anime_website/src/components/HomeBox.js
@@ -22,30 +22,30 @@ function HomeBox(){
 	return(
 		<>
 			<ul>
-				<li className={selectedBox1?"firstBoxHover":"firstBox"}  
-					onMouseEnter={()=>{setTitle("image 1");setSelectedBox1(true)}} 
+				<li className={selectedBox1?"firstBoxHover":"firstBox"}
+					onMouseEnter={()=>{setTitle("image 1");setSelectedBox1(true)}}
 					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox1(false)}}
 				>
 					<img className="top-li" src={image1} alt="" />
 				</li>
 				<li className={selectedBox2?"secondBoxHover":"secondBox"}
-					onMouseEnter={()=>{setTitle("image 2");setSelectedBox2(true)}} 
+					onMouseEnter={()=>{setTitle("image 2");setSelectedBox2(true)}}
 					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox2(false)}}
 				>
 					<img src={image2} alt="" />
 				</li>
 				<li className={selectedBox3?"thirdBoxHover":"thirdBox"}
-					onMouseEnter={()=>{setTitle("image 3");setSelectedBox3(true)}} 
+					onMouseEnter={()=>{setTitle("image 3");setSelectedBox3(true)}}
 					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox3(false)}}
 				>
 					<img className="top-li" src={image3} alt="" />
 				</li>
 				<li className={selectedBox4?"fourthBoxHover":"fourthBox"}
-					onMouseEnter={()=>{setTitle("image 4");setSelectedBox4(true)}} 
+					onMouseEnter={()=>{setTitle("image 4");setSelectedBox4(true)}}
 					onMouseLeave={()=>{setTitle('Our Items');setSelectedBox4(false)}}
 				>
 					<img src={image4} alt="" />
-				</li> 
+				</li>
 			</ul>
 			<h2 className="title">{title}</h2>
 			<div className="sound" onClick={handleSound}
