3ddcb9881c8cd3b692514bed83f6d63de56f419a
mahdi <m.karimzadeh@ehsan-group.com>
2021-10-01 16:45:26 +0330

    Arrange items on home page.

--- anime_website/src/components/HomeBox.js
+++ anime_website/src/components/HomeBox.js
@@ -28,9 +28,9 @@ function HomeBox(){
 
 	return(
 		<>
-			<ul className="homeBox">
-				<li className={selectedBox1?"box firstBoxHover":"firstBox"}  
-					onMouseEnter={()=>{setTitle("Videos");setSelectedBox1(true);}} 
+			<ul>
+				<li className="imageBox"
+					onMouseEnter={()=>{setTitle("Production");setSelectedBox1(true);}}
 					onMouseLeave={()=>{setTitle('Services');setSelectedBox1(false);}}
 				>
 				{selectedBox1?
@@ -106,7 +106,7 @@ function HomeBox(){
 			</ul>
 			<h2 className="title">{title}</h2>
 			<div className="sound" onClick={handleSound}
-				onMouseEnter={()=>setDrow(true)} 
+				onMouseEnter={()=>setDrow(true)}
 				onMouseLeave={()=>setDrow(false)}
 			>
 				{soundOff?
@@ -115,12 +115,12 @@ function HomeBox(){
 				}
 				{drow &&
 					<svg className="svg" xmlns="http://www.w3.org/2000/svg" >
-						<path class="drawCircle" 
-							d="M33.72 1.25A28.4 28.4 0 0012.43 4.7a22.6 22.6 0 00-7.77 7.75 21.33 21.33 0 00-1.81 16.69 22.75 22.75 0 0014.47 14.8 22.79 22.79 0 0020.42-3.41 22.79 22.79 0 008.89-18.7A20.86 20.86 0 0035.88 4.29C29.14.77 20.89 2 14.4 5.5 8.23 8.82 3.05 14.32 1.2 21.22a19.9 19.9 0 00-.46 2.14 15.44 15.44 0 001 8.93 19.1 19.1 0 001 1.92" 
-							fill="none" 
-							stroke="#231f20" 
-							stroke-linecap="round" 
-							stroke-linejoin="round" 
+						<path class="drawCircle"
+							d="M33.72 1.25A28.4 28.4 0 0012.43 4.7a22.6 22.6 0 00-7.77 7.75 21.33 21.33 0 00-1.81 16.69 22.75 22.75 0 0014.47 14.8 22.79 22.79 0 0020.42-3.41 22.79 22.79 0 008.89-18.7A20.86 20.86 0 0035.88 4.29C29.14.77 20.89 2 14.4 5.5 8.23 8.82 3.05 14.32 1.2 21.22a19.9 19.9 0 00-.46 2.14 15.44 15.44 0 001 8.93 19.1 19.1 0 001 1.92"
+							fill="none"
+							stroke="#231f20"
+							stroke-linecap="round"
+							stroke-linejoin="round"
 							></path>
 					</svg>
 				}
