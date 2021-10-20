c67510560364d82aa41e3e80d16ed4611ab75ab1
mahdi <m.karimzadeh@ehsan-group.com>
2021-10-20 14:19:08 +0330

    Admin Panel

---
+++ anime_website/src/components/AdminSideMenu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Reports',
+    path: '/reports',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Products',
+    path: '/products',
+    cName: 'nav-text'
+  },
+];
