778bf9843e90cef2c9bf0ef2db0e86301a6984ed
m.karbasi <karbasimahtab@gmail.com>
2021-11-06 18:28:32 +0330

    bugs

--- anime_website/src/screens/services/Training.js
+++ anime_website/src/screens/services/Training.js
@@ -51,13 +51,20 @@ export default function Products(props){
 	}, [])
 
 	const updateSize = ()=>{
-		const elWidth = document.querySelectorAll("#top-video .react-player")[0].offsetWidth; 
-		const elDivArrows = document.querySelectorAll(".team-column")[0].offsetHeight;
-		console.log('elDiveArrows', elDivArrows);
-		setVideoWidth(elWidth)
-		const videosWidth = parseInt(elWidth/5 -20);
-		setVideoSides(videosWidth);
-		setArrowTop(elDivArrows/3)
+		try{
+			let elWidth = document.querySelectorAll("#top-video .react-player"); 
+			let elDivArrows = document.querySelectorAll(".team-column");
+			if(elWidth){
+				elWidth = elWidth[0].offsetWidth
+				setVideoWidth(elWidth)
+				const videosWidth = parseInt(elWidth/5 -20);
+				setVideoSides(videosWidth);
+			}
+			if(elDivArrows){
+				elDivArrows = elDivArrows[0].offsetHeight
+				setArrowTop(elDivArrows/3)
+			}
+		}catch{}
 	}
 
 	const getProducts = ()=>{
