056c416ac376138fa0936d23a927bb24c91149d1
mahdi <m.karimzadeh@ehsan-group.com>
2021-11-13 21:24:19 +0330

    home boxes slide smoothly

--- anime_website/src/components/HomeBox.js
+++ anime_website/src/components/HomeBox.js
@@ -39,7 +39,7 @@ function HomeBox(){
 			<div className="home-container">
 				<div>
 					<ul className="home-box" >
-						<li className="image-box"
+						<li className="image-box first-image-box-show"
 							onMouseEnter={()=>{changeTitle("Production");setSelectedBox1(true);}}
 							onMouseLeave={()=>{changeTitle('Services');setSelectedBox1(false);}}
 						>
@@ -59,7 +59,7 @@ function HomeBox(){
 								}
 							</Link>
 						</li>
-						<li className="image-box"
+						<li className="image-box second-image-box-show"
 							onMouseEnter={()=>{changeTitle("Social Media");setSelectedBox2(true)}}
 							onMouseLeave={()=>{changeTitle('Services');setSelectedBox2(false)}}
 						>
@@ -79,7 +79,7 @@ function HomeBox(){
 								}
 							</Link>
 						</li>
-						<li className="image-box"
+						<li className="image-box third-image-box-show"
 							onMouseEnter={()=>{changeTitle("Graphics");setSelectedBox3(true)}}
 							onMouseLeave={()=>{changeTitle('Services');setSelectedBox3(false)}}
 						>
@@ -99,7 +99,7 @@ function HomeBox(){
 								}
 							</Link>
 						</li>
-						<li className="image-box"
+						<li className="image-box forth-image-box-show"
 							onMouseEnter={()=>{changeTitle("Training");setSelectedBox4(true)}}
 							onMouseLeave={()=>{changeTitle('Services');setSelectedBox4(false)}}
 						>
