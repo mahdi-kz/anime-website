54514cc0f969f2e28e171866e8c8902683ef0945
m.karbasi <karbasimahtab@gmail.com>
2021-12-18 20:35:02 +0330

    home icons & some changes

--- anime_website/src/components/Navbar.js
+++ anime_website/src/components/Navbar.js
@@ -3,19 +3,19 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-grid-system';
 import './Navbar.css';
 import Blog from './Blog';
-import blogIcone from '../images/icons/blog.webp';
-import blogo from '../images/logo/black-logo.svg';
-import wlogo from '../images/logo/white-logo.svg';
+import blogIcon from '../images/icons/blog.webp';
+import b_logo from '../images/logo/black-logo.svg';
+import w_logo from '../images/logo/white-logo.svg';
 import ServicesButtonNav from '../components/ServicesButtonNav.js'
 
 function GreiNavbar(props){
 	const [click, setClick] = useState(false);
-	const [drow, setDrow] = useState(false);
+	const [drew, setDrew] = useState(false);
 	const [menuHover, setMenuHover] = useState(false);
-	const [blogVisible, setblogVisible] = useState(false);
+	const [blogVisible, setBlogVisible] = useState(false);
 
 	const handleClick = ()=>setClick(!click);
-	const changingBlogVisible = ()=>setblogVisible(!blogVisible);
+	const changingBlogVisible = ()=>setBlogVisible(!blogVisible);
 	const closeModalMenu = ()=>setClick(false);
 	const showLogo = props.showLogo!==undefined?props.showLogo:true;
 
@@ -28,29 +28,32 @@ function GreiNavbar(props){
 							<div 
 								onClick={changingBlogVisible}
 								className={click?'hide':'navbar-bell'}
-								onMouseEnter={()=>setDrow(true)} 
-								onMouseLeave={()=>setDrow(false)}
+								onMouseEnter={()=>setDrew(true)}
+								onMouseLeave={()=>setDrew(false)}
 							>
-								<img alt="grei" src={blogIcone} className="blog-icon"/>
+								<img alt="grei" src={blogIcon} className="blog-icon"/>
+								{(drew && window.innerWidth>1200 ) &&
+                                    <svg className="nav-svg" viewBox='-5 -5 1200 1200'>
+                                        <path class="drawCircle"
+                                            d="M33.72 1.25A28.4 28.4 0 0012.43 4.7a22.6 22.6 0 00-7.77 7.75 21.33 21.33 0 00-1.81 16.69 22.75 22.75 0 0014.47 14.8 22.79 22.79 0 0020.42-3.41 22.79 22.79 0 008.89-18.7A20.86 20.86 0 0035.88 4.29C29.14.77 20.89 2 14.4 5.5 8.23 8.82 3.05 14.32 1.2 21.22a19.9 19.9 0 00-.46 2.14 15.44 15.44 0 001 8.93 19.1 19.1 0 001 1.92"
+                                            fill="none"
+                                            stroke="#231f20"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            ></path>
+                                    </svg>
+                                }
 							</div>
-							{ drow &&
-								<svg className="nav-svg">
-									<path class="drawCircle" 
-										d="M33.72 1.25A28.4 28.4 0 0012.43 4.7a22.6 22.6 0 00-7.77 7.75 21.33 21.33 0 00-1.81 16.69 22.75 22.75 0 0014.47 14.8 22.79 22.79 0 0020.42-3.41 22.79 22.79 0 008.89-18.7A20.86 20.86 0 0035.88 4.29C29.14.77 20.89 2 14.4 5.5 8.23 8.82 3.05 14.32 1.2 21.22a19.9 19.9 0 00-.46 2.14 15.44 15.44 0 001 8.93 19.1 19.1 0 001 1.92"
-										fill="none"
-										stroke="#231f20" 
-										stroke-linecap="round" 
-										stroke-linejoin="round" 
-										></path>
-								</svg>
-							}
+
+
+
 						</div>
 						{ blogVisible &&
 							<Blog changingBlogVisible={changingBlogVisible} />
 						}
 						{(showLogo || click) &&
 							<div className="logo-div">
-								<img alt="grei" src={click?wlogo:blogo} className='logo'/>
+								<img alt="grei" src={click?w_logo:b_logo} className='logo'/>
 							</div>
 						}
 						<div 
