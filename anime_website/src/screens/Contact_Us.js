3855b543288ce2b59597db3a72c86fa1669f1c86
m.karbasi <karbasimahtab@gmail.com>
2022-01-19 12:56:08 +0330

    changing css and solving menu icon problem in about-us

--- anime_website/src/screens/Contact_Us.js
+++ anime_website/src/screens/Contact_Us.js
@@ -7,12 +7,19 @@ import image1 from '../images/contact/contact2.webp';
 import backgroundImage from '../images/background/contact-bg.webp';
 
 export default function Contact_Us(){
-    const [showLogo, setShowLogo] = useState(true)
+    const [showLogo, setShowLogo] = useState(true);
+    const [offsetVal, setOffsetVal] = useState(0);
 
     useEffect(()=>{
         window.addEventListener('scroll',()=>{
 			setShowLogo(document.documentElement.scrollTop?false:true)
 		})
+		const headText = document.getElementById("top-info");
+		if(headText){
+		    if(350<window.innerWidth && window.innerWidth<400) setOffsetVal(headText.offsetLeft*2);
+		    else if(window.innerWidth<350) setOffsetVal(headText.offsetLeft);
+		    else setOffsetVal(headText.offsetLeft*2+20);
+		}
     },[])
 
     return(
@@ -46,25 +53,34 @@ export default function Contact_Us(){
                             }
                             <Col className="contact-box-info" xs={12} sm={6} md={7} xl={7} style={{marginTop: "5%"}}>
                                 <div className="contact-top-box">
-                                    <div className="contact-us-description">
-                                        <span>{config.description}</span>
+                                    <div  className="contact-us-description">
+                                        <span id="top-info">{config.description}</span>
                                     </div>
-                                    <div className="contact-us-info" 
-                                        onClick={()=>window.open(config.map_address, "_blank")}>
-                                        <i class="fa fa-map-marker fa-sm" aria-hidden="true"></i>
-                                        <span>{config.address}</span>
-                                    </div>
-                                    <div className="contact-us-info"
-                                        onClick={()=>window.open(config.mail_address, "_blank")}>
-                                        <i class="fa fa-envelope fa-sm" aria-hidden="true"></i>
-                                        <span>{config.email}</span>
-                                    </div>
-                                    <div className="contact-us-info">
-                                        <i class="fa fa-volume-control-phone fa-sm" aria-hidden="true"></i>
-                                        <span>{config.phone}</span>
+                                    <div style={{
+                                        textAlign:window.innerWidth<576?"left":"",
+                                        marginLeft:window.innerWidth<576?offsetVal:""
+                                    }}>
+                                        <div className="contact-us-info"
+                                            onClick={()=>window.open(config.map_address, "_blank")}>
+                                            <i class="fa fa-map-marker fa-sm" aria-hidden="true"></i>
+                                            <span>{config.address}</span>
+                                        </div>
+                                        <div className="contact-us-info"
+                                            onClick={()=>window.open(config.mail_address, "_blank")}>
+                                            <i class="fa fa-envelope fa-sm" aria-hidden="true"></i>
+                                            <span>{config.email}</span>
+                                        </div>
+                                        <div className="contact-us-info">
+                                            <i class="fa fa-volume-control-phone fa-sm" aria-hidden="true"></i>
+                                            <span>{config.phone}</span>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="contact-bottom-box">
+                                <div className="contact-bottom-box"
+                                     style={{
+                                        textAlign:window.innerWidth<576?"left":"",
+                                        marginLeft:window.innerWidth<576?offsetVal:""
+                                    }}>
                                     <div className="contact-us-info"
                                         onClick={()=>window.open(config.web_address, "_blank")}>
                                         <i class="fa fa-globe fa-sm" aria-hidden="true"></i>
